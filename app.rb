700a80d4d6967b4b4be89c05ad9f912c2eca4ee5
Laurence <ltaylorapp@gmail.com>
2019-05-14 15:06:30 +0100

    Added cat picture with border

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   "hello!"
 end
 
 get '/secret' do
   "this is a secret message"
+end
+
+get '/secret2' do
+  "this is another secret message"
+end
+
+get '/secret/deep' do
+  "this is a deep secret"
+end
+
+get '/cat' do
+  "<div style='border: dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
